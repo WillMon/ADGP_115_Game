bab99d11658734cd9639da3458477ed0d2dc7d94
WillMon <w.montero34@gmail.com>
2016-01-27 16:33:58 -0600

    Revert "Will Mon 6th Commit"
    This reverts commit 42f6067289bc63a525f92a49c8f6a3129a95949c.

--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -8,21 +8,11 @@ public class HealthScript : MonoBehaviour {
     public int startingHealth = 100;
     public int currentHealth;
     public Slider healthSlider;
-
-    //Animator anim;
-    PlayerMovement PlayerMove;
     
-
-    bool isDead;
-    bool TakesDmg;
 
 	// Use this for initialization
 	void Start ()
     {
-        //anim = GetComponent<Animator>();
-        PlayerMove = GetComponent<PlayerMovement>();
-
-
         currentHealth = startingHealth;
 	}
 	
@@ -34,24 +24,14 @@ public class HealthScript : MonoBehaviour {
 
         //Set the health bar's value to the current health
         healthSlider.value = currentHealth;
-
-        
-        if (healthSlider.value == 0)
-        {
-            Dead();
-        }
     }
-    void Dead()
-    {
-        isDead = true;
 
-        //anim.SetTrigger("Die");
-
-        PlayerMove.enabled = false;
-    }
     //// Update is called once per frame
     void Update()
     {
-      
+        if(healthSlider.value == 0)
+        {//Player Dies Here
+
+        }
     }
 }
